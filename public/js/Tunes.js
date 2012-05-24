870701825e94892104bd52f96f5f96b1ff91a887
DrummerHead <drummerhead@gmail.com>
2012-05-24 17:40:39 -0300

    Add Routers

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -25,6 +25,7 @@ window.Albums = Backbone.Collection.extend({
   url : '/albums'
 });
 
+window.library = new Albums();
 
 window.AlbumView = Backbone.View.extend({
 
@@ -74,6 +75,38 @@ window.LibraryView = Backbone.View.extend({
     return this
   }
 });
+
+window.BackboneTunes = Backbone.Router.extend({
+  routes : {
+    '' : 'home',
+    'blank' : 'blank'
+  },
+
+  initialize : function(){
+    this.libraryView = new LibraryView({
+      collection : window.library
+    });
+  },
+
+  home : function(){
+    var $container = $('#container');
+    $container.empty();
+    $container.append(this.libraryView.render().el);
+  },
+
+  blank : function(){
+    var $container = $('#container');
+    $container.empty();
+    $container.text('blank');
+  }
+
+});
+
+$(function(){
+  window.App = new BackboneTunes();
+  Backbone.history.start();
+})
+
 
 
 
